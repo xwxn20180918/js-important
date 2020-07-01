45c0ff50add2531a815f4950b367f6e1e5a579b9
xiongwen <2020@qq.com>
2020-07-01 18:25:56 +0800

    js交互

---
+++ 经典案例/7-有项目/04-swiper插件的使用-百分比布局/js/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function() {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          
+        //   dynamicBullets: true, 按钮的动画
+        },
+        // navigation: { //arrows
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        //   },
+      });
+})
